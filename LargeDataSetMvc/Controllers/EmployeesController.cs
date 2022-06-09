ed90b48c01202b2f2d44f1a528b6708194a43c73
leetrent <10787590+leetrent@users.noreply.github.com>
2022-06-09 06:35:08 -0500

    Data retrieval dev (#2)
    * Wired up MySQL driver
    
    * Able to retrieve rows from a view in the employees database

---
+++ LargeDataSetMvc/Controllers/EmployeesController.cs
@@ -0,0 +1,48 @@
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using LargeDataSetMvc.Model;
+using LargeDataSetMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace LargeDataSetMvc.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly EmployeeContext _dbContext;
+
+        public EmployeesController(EmployeeContext dbc)
+        {
+            _dbContext = dbc;
+        }
+
+        public ActionResult Employees_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            string logSnippet = "[EmployeeController][Employees_Read] =>";
+
+            Console.WriteLine($"{logSnippet} (_dbContext == null): {_dbContext == null}");
+
+            this.test();
+
+            var result = Enumerable.Range(0, 50).Select(i => new OrderViewModel
+            {
+                OrderID = i,
+                Freight = i * 10,
+                OrderDate = new DateTime(2016, 9, 15).AddDays(i % 7),
+                ShipName = "ShipName " + i,
+                ShipCity = "ShipCity " + i
+            });
+
+            var dsResult = result.ToDataSourceResult(request);
+            return Json(dsResult);
+        }
+
+        private void test()
+        {
+            string logSnippet = "[EmployeeController][test] =>";
+
+            IList<CurrentEmployee> currentEmployees = _dbContext.CurrentEmployees.ToList();
+            Console.WriteLine($"{logSnippet} (currentEmployees.Count): {currentEmployees.Count}");
+        }
+    }
+}
